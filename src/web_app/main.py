4846f0f764ebdf1a493f6f27bfff303cb2fda1f1
Akshat Garg <akshatdotgarg@gmail.com>
2025-06-16 12:38:02 -0700

    completed front end design on local server

--- src/web_app/main.py
+++ src/web_app/main.py
@@ -1,19 +1,23 @@
 # main entry point for the app, 
-
-
-
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
 import subprocess
 import sys
 
-from src.web_app.routes.evaluation_route import route as eval_route
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from src.web_app.routes import primary_page, evaluation_route
 
 app = FastAPI()
 
-app.include_router(eval_route)
+# Static files and templates
 app.mount("/static", StaticFiles(directory="src/web_app/static"), name="static")
+templates = Jinja2Templates(directory="src/web_app/static/html")
+
+# Include routes
+app.include_router(primary_page.router)
+app.include_router(evaluation_route.router)
+
 
 def run_uvicorn(host="127.0.0.1", port=8060, reload=True):
     url = f"http://{host}:{port}"
