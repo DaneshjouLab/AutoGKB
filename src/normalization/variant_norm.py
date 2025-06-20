0b7d1a1956777c0438b48b61242d33eb978ea278
gtcha2 <aron7628@gmail.com>
2025-06-20 12:37:13 -0700

    variant norm inital

---
+++ src/normalization/variant_norm.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+# router??
+# create class that handles routing the following
+# consolidate apis, accordingly to the following principles, 
+# type of information we want to validate
+# type of request we want want to verify....
+
+
+
+
+
+# Function input being variant -> process match correctness...
+# returns possible matches
+
+
+# function verify nomenclature
+# input string-> identify which nomenclature has highest probability
+
+def verify_nomenclature():
+    pass
+
+
+# variant normalizatio i
+
+#NOTES: variant 
+# normatlization
+# ok the following shoud do the following then 
+
+# function API request for normalization...
+# input : str variant RSID
+def api_requests():
+    # ask for 
+
+    pass
+
+## the following, https://api.ncbi.nlm.nih.gov/variation/v0/refsnp/{rsid}
+
+
+
+https://pmc.ncbi.nlm.nih.gov/articles/PMC6289290/
