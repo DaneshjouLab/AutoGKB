750be09864d4644031a4fa66ac578218dc467460
gtcha2 <aron7628@gmail.com>
2025-08-04 11:09:05 -0700

    drug ontology initial

---
+++ src/ontology_module/drug_ontology.py
@@ -0,0 +1,17 @@
+
+from variant_ontology import BaseNormalizer
+import requests
+
+# how to use, you have thew following, 
+
+
+
+class DrugNormalizer(BaseNormalizer):
+    def __init__(self):
+
+        ##
+        pass
+    def lookup_drug():
+        request.get()
+        
+        # the followig 
